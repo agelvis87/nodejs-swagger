c6f1e3a0da233bc453f4027fa7b06ca6f0e209b5
Agelvis Carreño <35966881+agelvis87@users.noreply.github.com>
2019-11-12 15:21:32 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 // Declare server directory
 process.env.SERVER_DIR = __dirname;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
 app.use(require("./config/swagger"));
 app.use(require("./routes/customer"));
 
 app.listen(3000, () => console.log(`App listening on port 3000`));
+
